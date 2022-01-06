51f253528ae8e0ff27dfac263c391087401febc7
robertj <robertjulian01@gmail.ciom>
2022-01-06 14:30:50 -0600

    functioned random card / html button

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 let values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'] //13 different cards per suit
 let suits = ['clubs', 'diams', 'hearts', 'spades'] //4 suits
 let deck = []
-let randomNum = Math.floor((Math.random()*52)+1) //for selecting random card
 //console.log(randomNum)
 
 let cardArea = document.getElementById('cardArea')
@@ -27,4 +26,8 @@ let cardArea = document.getElementById('cardArea')
         } 
     }   
 console.log(deck)
-cardArea.innerHTML += "<span style='color:" + deck[randomNum].suitColor + "'>&" + deck[randomNum].symbol + ";" + deck[randomNum].weight + "</span>" //test to see random card, showing colored symbol and weight, occassionally get an error, probably because of deck[]random mashups? Not sure yet
+
+function dealACard() {
+    let randomNum = Math.floor((Math.random()*52)+1) //for selecting random card
+    cardArea.innerHTML += "<span style='color:" + deck[randomNum].suitColor + "'>&" + deck[randomNum].symbol + ";" + deck[randomNum].weight + "</span>" //test to see random card, showing colored symbol and weight, occassionally get an error, probably because of deck[]random mashups? Not sure yet
+}
