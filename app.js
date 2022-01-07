a84e4d4724bbd62b02d0f598bc97533c3f4bbe2a
robertj <robertjulian01@gmail.ciom>
2022-01-07 00:19:27 -0600

    create function for intial 2 card deal

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'] //13 different cards per suit
 let suits = ['clubs', 'diams', 'hearts', 'spades'] //4 suits
 let deck = []
+let cardCount = 0 //track cards delt from deck
 let playerCards = []
 let dealerCards = []
 let cardArea = document.getElementById('cardArea')
@@ -29,12 +30,15 @@ let dealerHand = document.getElementById('dealerHand')
     }   
 console.log(deck)
 
+
 function dealACard() {
     let randomNum = Math.floor((Math.random()*52)+1) //for selecting random card
-    playerHand.innerHTML += "<span style='color:" + deck[randomNum].suitColor + "'>&" + deck[randomNum].symbol + ";" + deck[randomNum].weight + "</span>" //test to see random card, showing colored symbol and weight, occassionally get an error, probably because of deck[]random mashups? Not sure yet
+    playerHand.innerHTML += "<span style='color:" + deck[cardCount].suitColor + "'>&" + deck[cardCount].symbol + ";" + deck[cardCount].weight + "</span>" //test to see random card, showing colored symbol and weight, occassionally get an error, probably because of deck[]random mashups? Not sure yet
 
-    dealerHand.innerHTML += "<span style='color:" + deck[randomNum].suitColor + "'>&" + deck[randomNum].symbol + ";" + deck[randomNum].weight + "</span>"
+    dealerHand.innerHTML += "<span style='color:" + deck[cardCount].suitColor + "'>&" + deck[cardCount].symbol + ";" + deck[cardCount].weight + "</span>"
 }
+
+
 
 function shuffle(arr) { //randomly mixes up cards into different indices
     for(i = arr.length -1; i > 0; i--) { //loops through array back to front
@@ -46,3 +50,30 @@ function shuffle(arr) { //randomly mixes up cards into different indices
     return arr
 }
 // console.log(shuffle(deck))
+
+
+function dealNew() { //to initally deal 2 cards each
+    playerCards = []
+    dealerCards = []
+    playerHand.innerHTML = ''
+    dealerHand.innerHTML = ''
+    for(d = 0; d < 2; d++) {
+        dealerCards.push(deck[cardCount])
+        dealerHand.innerHTML += cardDisplay(cardCount)
+        cardCount++
+        playerCards.push(deck[cardCount])
+        playerHand.innerHTML += cardDisplay(cardCount)
+        cardCount++
+    }
+}
+//need to figure out how to hide first dealer card though
+
+function cardDisplay(cC) { //for outputting the generated card value - (cardCount)
+    return "<span style='color:" + deck[cardCount].suitColor + "'>&" + deck[cardCount].symbol + ";" + deck[cardCount].weight + "</span>"
+}
+
+function start() {
+    shuffle(deck)
+    dealNew(deck)
+}
+
