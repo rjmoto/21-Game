abef0218df270a42b8a8c558f3f60120f3d15d3e
robertj <robertjulian01@gmail.ciom>
2022-01-06 22:46:02 -0600

    create card deck shuffling function

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 let values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'] //13 different cards per suit
 let suits = ['clubs', 'diams', 'hearts', 'spades'] //4 suits
 let deck = []
-//console.log(randomNum)
-
+let playerCards = []
+let dealerCards = []
 let cardArea = document.getElementById('cardArea')
+let playerHand = document.getElementById('playerHand')
+let dealerHand = document.getElementById('dealerHand')
 
 
     for (s = 0; s < suits.length; s++) //loop puts together suits, values, color, card suit symbols
@@ -22,12 +24,25 @@ let cardArea = document.getElementById('cardArea')
                 value: values[v],
                 weight: weight
             }
-            deck.push(card)
+            deck.push(card) //populates the deck with all 52 card objects
         } 
     }   
 console.log(deck)
 
 function dealACard() {
     let randomNum = Math.floor((Math.random()*52)+1) //for selecting random card
-    cardArea.innerHTML += "<span style='color:" + deck[randomNum].suitColor + "'>&" + deck[randomNum].symbol + ";" + deck[randomNum].weight + "</span>" //test to see random card, showing colored symbol and weight, occassionally get an error, probably because of deck[]random mashups? Not sure yet
+    playerHand.innerHTML += "<span style='color:" + deck[randomNum].suitColor + "'>&" + deck[randomNum].symbol + ";" + deck[randomNum].weight + "</span>" //test to see random card, showing colored symbol and weight, occassionally get an error, probably because of deck[]random mashups? Not sure yet
+
+    dealerHand.innerHTML += "<span style='color:" + deck[randomNum].suitColor + "'>&" + deck[randomNum].symbol + ";" + deck[randomNum].weight + "</span>"
 }
+
+function shuffle(arr) { //randomly mixes up cards into different indices
+    for(i = arr.length -1; i > 0; i--) { //loops through array back to front
+        let j = Math.floor(Math.random() * (1 + i))
+        let temp = arr[i]
+        arr[i] = arr[j]
+        arr[j] = temp
+    }
+    return arr
+}
+// console.log(shuffle(deck))
