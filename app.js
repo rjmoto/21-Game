41fb1a7846d9342c690908960799c8640fc0613e
robertj <robertjulian01@gmail.ciom>
2022-01-09 10:54:48 -0600

    resolve break

--- app.js
+++ app.js
@@ -8,11 +8,6 @@ let cardArea = document.getElementById('cardArea')
 let playerHand = document.getElementById('playerHand')
 let dealerHand = document.getElementById('dealerHand')
 
-function start() {
-    console.log("start function invoked")
-    shuffle(deck)
-    dealNew(deck)
-}
 
     for (s = 0; s < suits.length; s++) //loop puts together suits, values, color, card suit symbols
     {
@@ -35,19 +30,12 @@ function start() {
     }   
 console.log(deck)
 
-function shuffle(arr) { //randomly mixes up cards into different indices
-    console.log("shuffle function invoked")
-    for(i = arr.length -1; i > 0; i--) { //loops through array back to front
-        let j = Math.floor(Math.random() * (1 + i))
-        let temp = arr[i] //the ol' switcheraoo
-        arr[i] = arr[j]
-        arr[j] = temp
-    }
-    return arr
+function start() {
+    shuffle(deck)
+    dealNew(deck)
 }
 
 function dealNew() { //to initally deal 2 cards each
-    console.log('dealNew function invoked')
     playerCards = []
     dealerCards = []
     playerHand.innerHTML = ''
@@ -63,36 +51,47 @@ function dealNew() { //to initally deal 2 cards each
         playerHand.innerHTML += cardDisplay(cardCount, d)
         cardCount++
     }
-    // console.log("pressed")
+    console.log("pressed")
+}
+
+function dealACard() {
+    // let randomNum = Math.floor((Math.random()*52)+1) //for selecting random card
+    playerHand.innerHTML += "<span style='color:" + deck[cardCount].suitColor + "'>&" + deck[cardCount].symbol + ";" + deck[cardCount].weight + "</span>" //test to see random card, showing colored symbol and weight, occassionally get an error, probably because of deck[]random mashups? Not sure yet
+
+    dealerHand.innerHTML += "<span style='color:" + deck[cardCount].suitColor + "'>&" + deck[cardCount].symbol + ";" + deck[cardCount].weight + "</span>"
 }
 
 
-
-
-
-function regularDeal() {
-    console.log(deck, "regularDeal invoked")
-
-    for(d = 0; d < 2; d++) {
-        dealerCards.push(deck[cardCount])
-        dealerHand.innerHTML += cardDisplay(cardCount, d)
-        if(d == 0) {
-            dealerHand.innerHTML += '<div id="hide" style="left: 100px;"></div>'
-        }
-        cardCount++
-        playerCards.push(deck[cardCount])
-        playerHand.innerHTML += cardDisplay(cardCount, d)
-        cardCount++
+function shuffle(arr) { //randomly mixes up cards into different indices
+    for(i = arr.length -1; i > 0; i--) { //loops through array back to front
+        let j = Math.floor(Math.random() * (1 + i))
+        let temp = arr[i] //the ol' switcheraoo
+        arr[i] = arr[j]
+        arr[j] = temp
+    }
+    return arr
 }
+
 
 function cardDisplay(v, d) { //for outputting the generated card values and design - (cardCount)
-    console.log("cardDisplay function invoked")
     let cardPos = (d > 0) ? d * 60 + 100 : 100; //card relative positioning, styling, values
     return '<div class="card ' + deck[v].symbol + '" style="left:' + cardPos + 'px;"> <div class="cardTop suit">' + deck[v].value + '<br></div>  <div class="cardCenter suit"></div> <div class="cardBottom suit">' + deck[v].value + '<br></div> </div>'
 }
 
+// function start() {
+//     shuffle(deck)
+//     dealNew(deck)
+// }
+
+
+//continue dealing after game start
+//win/lose/push
+//bets
+//wallet
+//Aces
+
 function choice(hitStay) {
-    console.log("player pressed hit or stay")
+    console.log("pressed")
     if (hitStay == 'hit') {
         anotherCard()
     } if (hitStay == 'stay'){
@@ -108,13 +107,13 @@ function dealACard() {
     dealerHand.innerHTML += "<span style='color:" + deck[cardCount].suitColor + "'>&" + deck[cardCount].symbol + ";" + deck[cardCount].weight + "</span>"
 }
 
+
 function anotherCard() {
     console.log("deal player another card")
     playerCards.push(deck[cardCount])
     console.log(cardCount)
     playerHand.innerHTML += cardDisplay(cardCount, (playerCards.length - 1))
     cardCount++
-
 }
 
 function endTurn() {
@@ -122,10 +121,23 @@ function endTurn() {
 }
 
 function contDeal() { //to continue dealing after game/hand has already started
-    console.log('continue dealing cards')
+
 }
 
+// function regularDeal() {
+//     console.log(deck, "regularDeal invoked")
 
+//     for(d = 0; d < 2; d++) {
+//         dealerCards.push(deck[cardCount])
+//         dealerHand.innerHTML += cardDisplay(cardCount, d)
+//         if(d == 0) {
+//             dealerHand.innerHTML += '<div id="hide" style="left: 100px;"></div>'
+//         }
+//         cardCount++
+//         playerCards.push(deck[cardCount])
+//         playerHand.innerHTML += cardDisplay(cardCount, d)
+//         cardCount++
+// }
 
 
 
@@ -145,3 +157,27 @@ dealersHandTotal = document.getElementById('dealersHandTotal')
 //Aces
 //result message to player
 //show player & dealer's scores 
+
+
+// my screw up section back-up:
+
+
+// function dealNew() { //to initally deal 2 cards each
+//     playerCards = []
+//     dealerCards = []
+//     playerHand.innerHTML = ''
+//     dealerHand.innerHTML = ''
+//     for(d = 0; d < 2; d++) {
+//         dealerCards.push(deck[cardCount])
+//         dealerHand.innerHTML += cardDisplay(cardCount, d)
+//         if(d == 0) {
+//             dealerHand.innerHTML += '<div id="hide" style="left: 100px;"></div>'
+//         }
+//         cardCount++
+//         playerCards.push(deck[cardCount])
+//         playerHand.innerHTML += cardDisplay(cardCount, d)
+//         cardCount++
+//     }
+//     console.log("pressed")
+// }
+//need to figure out how to hide first dealer card though
